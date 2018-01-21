08e0e08006b7cea2f41e56ea4a7491f48d6dbf53
marciocleberbrazil <marciocleberbrazil@gmail.com>
2018-01-21 17:49:58 +1100

    Setup browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,69 @@
-var gulp = require('gulp'), $ = require('gulp-load-plugins')({ lazy: true });
-var karma = require('karma');
+var $ = require('gulp-load-plugins')({ lazy: true }),
+	args = require('yargs').argv,
+	browserSync = require('browser-sync').create(),
+	del = require('del'),
+	gulp = require('gulp'),
+	karma = require('karma');
+
+var config = {
+	isDebug: args.debug ? args.debug : false,
+	folder: {
+		test: './test/',
+		src: './src/',
+		dist: './dist/',
+	},
+	filename: {
+		js: 'app.min.js',
+		html: 'index.html'
+	}
+};
 
 /**
  * List the available gulp tasks
  */
 gulp.task('help', $.taskListing);
 gulp.task('default', ['help']);
+
+/**
+ * Copy Files
+ */
+gulp.task('build:copyfiles', function () {
+	return gulp.src([
+			'node_modules/angular/angular.min.js',
+			'node_modules/@uirouter/angularjs/release/angular-ui-router.min.js',
+			config.folder.src + 'app.css',
+			config.folder.src + config.filename.html
+		])
+		.pipe(gulp.dest(config.folder.dist));
+});
+
+/**
+ * Compile JavaScript files
+ */
+gulp.task('build:js', function () {
+	return gulp.src([
+			config.folder.src + '**/*.js', '!' + config.folder.src + '**/*.spec.js'
+		])
+		.pipe($.if(config.isDebug, $.debug()))
+		.pipe($.sourcemaps.init())
+		.pipe($.angularFilesort())
+		.pipe($.concat(config.filename.js))
+		.pipe(gulp.dest(config.folder.dist));
+});
+
+/**
+ * Clean Destination folder
+ */
+gulp.task('build:cleanup', function () {
+	return del(config.folder.dist);
+});
+
+/**
+ * Run all build process step by step
+ */
+gulp.task('build', ['build:cleanup'], function () {
+	gulp.start(['build:js', 'build:copyfiles']);
+});
 
 /**
  * Run tests with watcher
@@ -15,4 +73,27 @@ gulp.task('tests', function (done) {
 		configFile: __dirname + '/karma.conf.js',
 		singleRun: false
 	}, done).start();
+});
+
+/**
+ * Open Application in the Browser and start watcher
+ */
+gulp.task('start', ['build'], function () {
+	browserSync.init({
+        server: {
+            baseDir: config.folder.dist
+		},
+		files: [config.folder.dist + '/**/*.*'],
+		notify: true,
+        reloadDelay: 1000
+	});
+
+	gulp.watch(
+		[
+			config.folder.src + '**/*.js',
+			config.folder.src + '**/*.html',
+		],
+		['build']
+	)
+	.on('change', browserSync.reload);
 });
