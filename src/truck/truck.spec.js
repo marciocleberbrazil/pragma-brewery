1dfd898c4f422dcf67f8a696b85f781116a7f2b7
aqueo-fabiopereira <fpereira@aqueo.com.au>
2018-01-22 16:41:14 +1100

    Change truck controller and tests

--- src/truck/truck.spec.js
+++ src/truck/truck.spec.js
@@ -1,49 +1,22 @@
 describe('truck section', function () {
 
-	var truck, scope, interval;
+	var controller, scope, interval;
 
 	beforeEach(module('app.truck'));
 
-	beforeEach(inject(function (_$controller_, _$interval_) {
-		truck = _$controller_('Truck', {
+	beforeEach(inject(function (_$controller_) {
+		controller = _$controller_('Truck', {
 			$scope: scope
 		});
-		interval = _$interval_;
 	}));
 
 	describe('Close door', function () {
-		var currentTemperature;
-
-		beforeEach(function(){
-			currentTemperature = 5;
-			truck.currentTemperature = currentTemperature;
+		it('should exist increase the temperature', function(){
+			expect(controller.increase).toBeDefined();
 		});
 
-		it('should increase the temperature', function(){
-			truck.doorOpen = false;
-			truck.toggleDoor();
-			var timerStep = 0.5;
-			interval.flush(1000);
-			expect(truck.currentTemperature).toBe(currentTemperature + timerStep);
-		});
-
-		it('should decrease the temperature', function(){
-			truck.doorOpen = true;
-			truck.toggleDoor();
-			var timerStep = 0.5;
-			interval.flush(1000);
-			expect(truck.currentTemperature).toBe(currentTemperature - timerStep);
-		});
-
-		it('should not decrease minimal temperature', function(){
-			var minimalTemperature = 4;
-			currentTemperature = 4;
-			truck.currentTemperature = currentTemperature;
-
-			truck.doorOpen = true;
-			truck.toggleDoor();
-			interval.flush(1000);
-			expect(truck.currentTemperature).toBe(minimalTemperature);
+		it('should exist decrease the temperature', function(){
+			expect(controller.decrease).toBeDefined();
 		});
 	});
 });
